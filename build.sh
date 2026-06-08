2111780249bc3f2ed4e602153be3007e3ab4e681
djamic <djamic@gmail.com>
2026-06-08 10:20:55 +0500

    Add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -eu
+
+OUT=${1:-wireguard_webmin.wbm.gz}
+TMP=${TMPDIR:-/tmp}/wireguard_webmin_build.$$
+
+rm -rf "$TMP"
+mkdir -p "$TMP"
+
+tar \
+  --exclude='./.git' \
+  --exclude='./*.wbm.gz' \
+  --exclude='./*.tar.gz' \
+  -cf - . | tar -xf - -C "$TMP"
+
+tar -czf "$OUT" -C "$TMP" .
+rm -rf "$TMP"
+
+echo "Built $OUT"
